dc1390ee31b1d2f06d9683cd68659f908ff8e266
Abduquddus1998 <33756692+Abduquddus1998@users.noreply.github.com>
2019-05-23 15:41:40 +0500

    full version

---
+++ config/password.js
@@ -0,0 +1,31 @@
+const localStrategy = require('passport-local').Strategy;
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+//Load User Model
+
+const User = require('../models/User');
+
+module.exports = function(passport){
+    passport.use(
+        new localStorage({username: "email"}, (email,password,done) => {
+            //Match User
+            User.findOne({email: email})
+            .then(user => {
+                if(!user){
+                    return done(null, false, {message: 'That email is not registrated'});
+                }
+                //Match password
+                bcrypt.compare(password, user.password, (err, isMatch) =>{
+                    if(err) throw err;
+                    if(isMatch){
+                        return done(null, user);
+                    } else {
+                        return done(null, false, {message: 'Password incorrect' });
+                    }
+                });
+            })
+            .catch(err => console.log(err));
+        })
+    )
+}
